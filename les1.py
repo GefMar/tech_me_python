2107b35adf7bedff63010eed178eb17a63ce725a
SARomanchuk <SARomanchuk@sberbank.ru>
2021-09-30 00:32:49 +0300

    first commit

---
+++ les1.py
@@ -0,0 +1,71 @@
+"""
+snake_case
+kebab-case
+camelCase
+CamelCase
+"""
+
+"""
+int - integer type ЦЕЛОЕ ЧИСЛО
+float - float type Число с плавающей точкой
+str - string type Строки
+bool - boolean type Булевое (Истина или Лож)
+"""
+"""
+Арифметические операторы
+
++
+-
+*
+/
+//
+%
+**
+"""
+
+"""
+Переменные
+Именованные ссылки
+snake_case - Нейминг Переменные
+SNAKE_CASE - НЕЙМИНГ КОНСТАНТЫ
+"""
+"""
+ВВОД и ВЫВОД ДАННЫХ
+
+"""
+# привет это однострочный коммент
+
+hello = "HELLO WORLD \n some"
+hello_2 = 'HELLO WORLD'
+hello_3 = """HELLO WORLD
+some
+"""
+hello_4 = '''HELLO WORLD
+some
+'''
+
+name = "Sergey"
+surname = "Romanchuk"
+
+fullname = name + " " + surname
+
+print(fullname, 1, 2, 3, 4, 5, 6)
+
+"""
+Логические операторы
+
+==
+>
+<
+=>
+<=
+!=
+"""
+'0b10110'
+"&"
+"0b00000"
+
+'0b10111'
+'0b00001'
+"0b00001"
+
